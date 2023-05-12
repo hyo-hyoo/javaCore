282500291be468e135de3e7b678f92dcf81a5c9a
hyo-hyoo <121388585+hyo-hyoo@users.noreply.github.com>
2023-05-12 23:36:21 +0800

    pull commit

--- u4/EmployeeTest.java
+++ u4/EmployeeTest.java
@@ -25,6 +25,7 @@ public class EmployeeTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
 
 /*
         for (Employee e : staff) {
